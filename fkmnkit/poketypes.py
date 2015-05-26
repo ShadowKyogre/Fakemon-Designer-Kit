dbcdd39cd70b0611f80166d9f65bd9566f5970f3
ShadowKyogre <shadowkyogre@aim.com>
2015-05-26 09:47:53 -0700

    Add base stat validation and start drafting validation for both Pokemon and instances of Pokemon. automated calculations for Pokemon

--- fkmnkit/poketypes.py
+++ fkmnkit/poketypes.py
@@ -7,8 +7,8 @@ gettext.install('fakemon_kit')
 
 class PokeMove:
 	PHYSICAL = 0
-	SPECIAL = 0
-	STATUS = 0
+	SPECIAL = 1
+	STATUS = 2
 	def __init__(self, label='', move_kind=0, bp=0, accuracy=100, efftype=None, stabtype=None):
 		pass
 
@@ -23,8 +23,11 @@ class PokeMove:
 			return 1
 
 class Pokemon:
+	MIN_BST_VAL=1
+	MAX_BST_VAL=256
 	def __init__(self, label='', dexno=0, regionno=0, genno=0, egggrps=None, 
-	             abilities=None, types=None, hp=1, atk=1, dfn=1, satk=1, sdfn=1, spd=1):
+	             abilities=None, types=None, bhp=MIN_BST_VAL, batk=MIN_BST_VAL, 
+	             bdfn=MIN_BST_VAL, bsatk=MIN_BST_VAL, bsdfn=MIN_BST_VAL, bspd=MIN_BST_VAL):
 		#hash should be calculated by dexno and genno due to intro of mega-evos
 
 		self.label = label
@@ -32,12 +35,12 @@ class Pokemon:
 		self.regionno = regionno
 		self.genno = genno
 
-		self.hp = hp
-		self.atk = atk
-		self.dfn = dfn
-		self.satk = satk
-		self.sdfn = sdfn
-		self.spd = spd
+		self.bhp = bhp
+		self.batk = batk
+		self.bdfn = bdfn
+		self.bsatk = bsatk
+		self.bsdfn = bsdfn
+		self.bspd = bspd
 
 		if types is None:
 			self.types = PokeTypeSet([])
@@ -53,6 +56,113 @@ class Pokemon:
 			self.abilities = []
 		else:
 			self.abilities = abilities
+
+	def _validate_bst_val(self, d):
+		return not (d < Pokemon.MIN_BST_VAL or d > Pokemon.MAX_BST_VAL)
+
+	@property
+	def bhp(self):
+		return self._bhp
+
+	@bhp.setter
+	def bhp(self, d):
+		if not self._validate_bst_val(d):
+			raise ValueError("Base stats cannot go below 1 or above 256!")
+		self._bhp = d
+
+	@property
+	def batk(self):
+		return self._batk
+
+	@batk.setter
+	def batk(self, d):
+		if not self._validate_bst_val(d):
+			raise ValueError("Base stats cannot go below 1 or above 256!")
+		self._batk = d
+
+	@property
+	def bdfn(self):
+		return self._bdfn
+
+	@bdfn.setter
+	def bdfn(self, d):
+		if not self._validate_bst_val(d):
+			raise ValueError("Base stats cannot go below 1 or above 256!")
+		self._bdfn = d
+
+	@property
+	def bsatk(self):
+		return self._bsatk
+
+	@bsatk.setter
+	def bsatk(self, d):
+		if not self._validate_bst_val(d):
+			raise ValueError("Base stats cannot go below 1 or above 256!")
+		self._bsatk = d
+
+	@property
+	def bsdfn(self):
+		return self._bsdfn
+
+	@bsdfn.setter
+	def bsdfn(self, d):
+		if not self._validate_bst_val(d):
+			raise ValueError("Base stats cannot go below 1 or above 256!")
+		self._bsdfn = d
+
+	@property
+	def bspd(self):
+		return self._bspd
+
+	@bspd.setter
+	def bspd(self, d):
+		if not self._validate_bst_val(d):
+			raise ValueError("Base stats cannot go below 1 or above 256!")
+		self._bspd = d
+
+class PokemonInstance:
+	MIN_LVL = 1
+	MAX_LVL = 100
+	MAX_IVS = 31
+	MAX_EVS_PER_STAT = 252
+	MAX_EVS_TOTAL = 510
+
+	def _validate_lvl(self, d):
+		return not (d < PokemonInstance.MIN_LVL or d > PokemonInstance.MAX_LVL)
+
+	def _validate_ev_ttl(self, d):
+		return (self.hpev + self.atkev + self.dfnev + self.satkev + self.sdfnev + self.spdev) <= PokemonInstance.MAX_EVS_TOTAL
+
+	def _validate_ev_val(self, d):
+		return d <= PokemonInstance.MAX_EVS_PER_STAT
+
+	def _validate_iv_val(self, d):
+		return not (d < PokemonInstance.MIN_BST_VAL or d > PokemonInstance.MAX_BST_VAL)
+
+	def _calculate_stat(self, iv, bsv, evs, offset=5, nature=0):
+		return ((iv + 2* bsv + evs/4) * self.level / 100 + offset) * (1.0 + nature * 0.1)
+
+	def attacks_with(self, technique, *targets):
+		damages = []
+
+		for target in targets:
+			if technique.move_kind == PokeMove.PHYSICAL and bp > 0:
+				a_d_ratio = self.attack / target.defense
+			elif technique.move_kind == PokeMove.SPECIAL and bp > 0:
+				a_d_ratio = self.sattack / target.sdefense
+			else:
+				damages.append(technique.custom_damage(*targets))
+				continue
+
+			#critical hits are not applied in here, they're to be calculated outside
+			#in an actual battle simulator
+
+			stab = 1.5 if technique.stabtype in self.types else 1
+			dmg_modifier = technique.efftype.effective_against(target.types)
+			dmg = ((2*self.level+10) * a_d_ratio * technique.bp + 2) * dmg_modifier * stab
+			damages.append((0.85*dmg, dmg))
+
+		return damages
 
 class PokeType:
 	def __init__(self, tid=0, weaknesses=None, resistances=None, immunities=None, label=''):
---
+++ tests/pokemon_creation.py
@@ -0,0 +1,15 @@
+import unittest
+
+import fkmnkit
+from fkmnkit.poketypes import Pokemon
+
+class TestPokemonCreation(unittest.TestCase):
+	def test_bst_validation(self):
+		with self.assertRaises(ValueError):
+			Pokemon(bhp=300)
+
+		actual_pokemon = Pokemon(bhp=200)
+		self.assertEquals(actual_pokemon.bhp, 200)
+
+if __name__ == '__main__':
+	unittest.main()
