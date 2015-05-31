93371f63febb6d5a9358a2b31391739679a5053f
ShadowKyogre <shadowkyogre@aim.com>
2015-05-30 23:37:58 -0700

    Start code for formula based BP which will eventually take the opposing Pokemon and defending Pokemon into account. See http://stackoverflow.com/questions/11951701/safe-way-to-parse-user-supplied-mathematical-formula-in-python for where the code came from.

--- fkmnkit/moves.py
+++ fkmnkit/moves.py
@@ -1,4 +1,23 @@
 from enum import Enum
+import ast
+
+AST_WHITELIST = (ast.Expression, ast.Call, ast.Name, ast.Load,
+                 ast.BinOp, ast.UnaryOp, ast.operator, ast.unaryop, ast.cmpop,
+                 ast.Num, ast.Tuple, ast.Attribute)
+EVAL_SAFEDICT = {k:locals().get(k, None) for k in ('range', 'tuple') }
+
+def eq_eval(eq, lcl_vrs):
+	lcl_vrs_cpy = lcl_vrs.copy()
+	lcl_vrs_cpy.update(EVAL_SAFEDICT)
+	return eval(eq, {"__builtins__": None})
+
+def verify_eq(user_eq):
+	tree = ast.parse(user_eq, mode='eval')
+	valid = all(isinstance(node, AST_WHITELIST) for node in ast.walk(tree))
+	if valid:
+		return compile(tree, filename='', mode='eval')
+	else:
+		raise ValueError("You can't use this for an equation!")
 
 class MoveCategory(Enum):
 	PHYSICAL = 0
@@ -6,7 +25,7 @@ class MoveCategory(Enum):
 	STATUS = 2
 
 class PokeMove:
-	def __init__(self, label='', move_cat=MoveCategory.PHYSICAL, bp=0, accuracy=100, efftype=None, stabtype=None):
+	def __init__(self, label='', move_cat=MoveCategory.PHYSICAL, bp="0", accuracy=100, efftype=None, stabtype=None):
 		pass
 
 	def applicable_stab(self, pkmn):
