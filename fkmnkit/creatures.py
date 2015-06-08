6323c4ea0b8b9f064807d54d14e1d3233dd4aee1
ShadowKyogre <shadowkyogre@aim.com>
2015-06-07 19:56:18 -0700

    Finish implementing proper stat representation, calculations, and test for those

--- fkmnkit/creatures.py
+++ fkmnkit/creatures.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from math import floor
+
 from fkmnkit.moves import MoveCategory
 from fkmnkit.poketypes import PokeTypeSet
 
@@ -57,7 +59,7 @@ class Pokemon:
 	@bhp.setter
 	def bhp(self, d):
 		if not self._validate_bst_val(d):
-			raise ValueError("Base stats cannot go below 1 or above 256!")
+			raise ValueError("Base stats cannot go below {} or above {}!".format(Pokemon.MIN_BST_VAL, Pokemon.MAX_BST_VAL))
 		self._bhp = d
 
 	@property
@@ -67,7 +69,7 @@ class Pokemon:
 	@batk.setter
 	def batk(self, d):
 		if not self._validate_bst_val(d):
-			raise ValueError("Base stats cannot go below 1 or above 256!")
+			raise ValueError("Base stats cannot go below {} or above {}!".format(Pokemon.MIN_BST_VAL, Pokemon.MAX_BST_VAL))
 		self._batk = d
 
 	@property
@@ -77,7 +79,7 @@ class Pokemon:
 	@bdfn.setter
 	def bdfn(self, d):
 		if not self._validate_bst_val(d):
-			raise ValueError("Base stats cannot go below 1 or above 256!")
+			raise ValueError("Base stats cannot go below {} or above {}!".format(Pokemon.MIN_BST_VAL, Pokemon.MAX_BST_VAL))
 		self._bdfn = d
 
 	@property
@@ -87,7 +89,7 @@ class Pokemon:
 	@bsatk.setter
 	def bsatk(self, d):
 		if not self._validate_bst_val(d):
-			raise ValueError("Base stats cannot go below {} or above {}!".format(MIN_BST_VAL, MAX_BST_VAL))
+			raise ValueError("Base stats cannot go below {} or above {}!".format(Pokemon.MIN_BST_VAL, Pokemon.MAX_BST_VAL))
 		self._bsatk = d
 
 	@property
@@ -97,7 +99,7 @@ class Pokemon:
 	@bsdfn.setter
 	def bsdfn(self, d):
 		if not self._validate_bst_val(d):
-			raise ValueError("Base stats cannot go below 1 or above 256!")
+			raise ValueError("Base stats cannot go below {} or above {}!".format(Pokemon.MIN_BST_VAL, Pokemon.MAX_BST_VAL))
 		self._bsdfn = d
 
 	@property
@@ -107,8 +109,24 @@ class Pokemon:
 	@bspd.setter
 	def bspd(self, d):
 		if not self._validate_bst_val(d):
-			raise ValueError("Base stats cannot go below 1 or above 256!")
+			raise ValueError("Base stats cannot go below {} or above {}!".format(Pokemon.MIN_BST_VAL, Pokemon.MAX_BST_VAL))
 		self._bspd = d
+
+class Nature:
+	def __init__(self, boost, detriment, boost_val=1, detriment_val=1):
+		self.boost = boost
+		self.boost_val = boost_val
+
+		self.detriment = detriment
+		self.detriment_val = detriment_val
+
+	def get_bonus(self, stat_id):
+		multi = 0
+		if stat_id == self.boost:
+			multi += self.boost_val
+		if stat_id == self.detriment:
+			multi -= self.detriment_val
+		return multi
 
 class PokemonInstance:
 	MIN_LVL = 1
@@ -119,8 +137,181 @@ class PokemonInstance:
 	MAX_STAT_STAGE = 6
 	MIN_STAT_STAGE = -6
 
-	def __init__(self, pokedex_data, hp_iv=0, atk_iv=0, dfn_iv=0, satk_iv=0, sdef_iv=0, spd_iv=0):
-		pass
+	def __init__(self, pokedex_data, hp_iv=0, atk_iv=0, dfn_iv=0, satk_iv=0, sdfn_iv=0, spd_iv=0, 
+	              hp_ev=0, atk_ev=0, dfn_ev=0, satk_ev=0, sdfn_ev=0, spd_ev=0,   
+	             current_hp=-1, nature=None):
+		self.pokedex_data = pokedex_data
+		self.nature = nature
+
+		self.hp_iv = hp_iv
+		self.atk_iv = atk_iv
+		self.dfn_iv = dfn_iv
+		self.satk_iv = satk_iv
+		self.sdfn_iv = sdfn_iv
+		self.spd_iv = spd_iv
+
+		self.hp_ev = hp_ev
+		self.atk_ev = atk_ev
+		self.dfn_ev = dfn_ev
+		self.satk_ev = satk_ev
+		self.sdfn_ev = sdfn_ev
+		self.spd_ev = spd_ev
+
+	@property
+	def hp(self):
+		return self._calculate_stat(self.hp_iv, self.pokedex_data.bhp, self.hp_ev, lvloffset=100, foffset=10)
+
+	@property
+	def atk(self):
+		return self._calculate_stat(self.atk_iv, self.pokedex_data.batk, self.atk_ev, nature=self.nature.get_bonus(StatID.ATK))
+
+	@property
+	def dfn(self):
+		return self._calculate_stat(self.dfn_iv, self.pokedex_data.bdfn, self.dfn_ev, nature=self.nature.get_bonus(StatID.DFN))
+
+	@property
+	def satk(self):
+		return self._calculate_stat(self.satk_iv, self.pokedex_data.bsatk, self.satk_ev, nature=self.nature.get_bonus(StatID.SATK))
+
+	@property
+	def sdfn(self):
+		return self._calculate_stat(self.sdfn_iv, self.pokedex_data.bsdfn, self.sdfn_ev, nature=self.nature.get_bonus(StatID.SDFN))
+
+	@property
+	def spd(self):
+		return self._calculate_stat(self.spd_iv, self.pokedex_data.bspd, self.spd_ev, nature=self.nature.get_bonus(StatID.SPD))
+
+	@property
+	def hp_iv(self):
+		return self._hp_iv
+
+	@hp_iv.setter
+	def hp_iv(self, d):
+		if not self._validate_iv_val(d):
+			raise ValueError("IVs cannot go below 0 or above {}!".format(PokemonInstance.MAX_IVS))
+		self._hp_iv = d
+
+	@property
+	def atk_iv(self):
+		return self._atk_iv
+
+	@atk_iv.setter
+	def atk_iv(self, d):
+		if not self._validate_iv_val(d):
+			raise ValueError("IVs cannot go below 0 or above {}!".format(PokemonInstance.MAX_IVS))
+		self._atk_iv = d
+
+	@property
+	def dfn_iv(self):
+		return self._dfn_iv
+
+	@dfn_iv.setter
+	def dfn_iv(self, d):
+		if not self._validate_iv_val(d):
+			raise ValueError("IVs cannot go below 0 or above {}!".format(PokemonInstance.MAX_IVS))
+		self._dfn_iv = d
+
+	@property
+	def satk_iv(self):
+		return self._satk_iv
+
+	@satk_iv.setter
+	def satk_iv(self, d):
+		if not self._validate_iv_val(d):
+			raise ValueError("IVs cannot go below 0 or above {}!".format(PokemonInstance.MAX_IVS))
+		self._satk_iv = d
+
+	@property
+	def sdfn_iv(self):
+		return self._sdfn_iv
+
+	@sdfn_iv.setter
+	def sdfn_iv(self, d):
+		if not self._validate_iv_val(d):
+			raise ValueError("IVs cannot go below 0 or above {}!".format(PokemonInstance.MAX_IVS))
+		self._sdfn_iv = d
+
+	@property
+	def spd_iv(self):
+		return self._spd_iv
+
+	@spd_iv.setter
+	def spd_iv(self, d):
+		if not self._validate_iv_val(d):
+			raise ValueError("IVs cannot go below 0 or above {}!".format(PokemonInstance.MAX_IVS))
+		self._spd_iv = d
+
+	@property
+	def hp_ev(self):
+		return self._hp_ev
+
+	@hp_ev.setter
+	def hp_ev(self, d):
+		if not self._validate_ev_val(d):
+			raise ValueError("EVs cannot go below 0 or above {}!".format(PokemonInstance.MAX_EVS_PER_STAT))
+		if not self._validate_ev_ttl(hp_ev=d):
+			raise ValueError("EV Values exceed maximum total!")
+		self._hp_ev = d
+
+	@property
+	def atk_ev(self):
+		return self._atk_ev
+
+	@atk_ev.setter
+	def atk_ev(self, d):
+		if not self._validate_ev_val(d):
+			raise ValueError("EVs cannot go below 0 or above {}!".format(PokemonInstance.MAX_EVS_PER_STAT))
+		if not self._validate_ev_ttl(atk_ev=d):
+			raise ValueError("EV Values exceed maximum total!")
+		self._atk_ev = d
+
+	@property
+	def dfn_ev(self):
+		return self._dfn_ev
+
+	@dfn_ev.setter
+	def dfn_ev(self, d):
+		if not self._validate_ev_val(d):
+			raise ValueError("EVs cannot go below 0 or above {}!".format(PokemonInstance.MAX_EVS_PER_STAT))
+		if not self._validate_ev_ttl(dfn_ev=d):
+			raise ValueError("EV Values exceed maximum total!")
+		self._dfn_ev = d
+
+	@property
+	def satk_ev(self):
+		return self._satk_ev
+
+	@satk_ev.setter
+	def satk_ev(self, d):
+		if not self._validate_ev_val(d):
+			raise ValueError("EVs cannot go below 0 or above {}!".format(PokemonInstance.MAX_EVS_PER_STAT))
+		if not self._validate_ev_ttl(satk_ev=d):
+			raise ValueError("EV Values exceed maximum total!")
+		self._satk_ev = d
+
+	@property
+	def sdfn_ev(self):
+		return self._sdfn_ev
+
+	@sdfn_ev.setter
+	def sdfn_ev(self, d):
+		if not self._validate_ev_val(d):
+			raise ValueError("EVs cannot go below 0 or above {}!".format(PokemonInstance.MAX_EVS_PER_STAT))
+		if not self._validate_ev_ttl(sdfn_ev=d):
+			raise ValueError("EV Values exceed maximum total!")
+		self._sdfn_ev = d
+
+	@property
+	def spd_ev(self):
+		return self._spd_ev
+
+	@spd_ev.setter
+	def spd_ev(self, d):
+		if not self._validate_ev_val(d):
+			raise ValueError("EVs cannot go below 0 or above {}!".format(PokemonInstance.MAX_EVS_PER_STAT))
+		if not self._validate_ev_ttl(spd_ev=d):
+			raise ValueError("EV Values exceed maximum total!")
+		self._spd_ev = d
 
 	@property
 	def level(self):
@@ -129,7 +320,7 @@ class PokemonInstance:
 	@level.setter
 	def level(self, d):
 		if not self._validate_lvl(d):
-			raise ValueError("Level must be between {} and {}".format(MIN_LVL, MAX_LVL))
+			raise ValueError("Level must be between {} and {}".format(PokemonInstance.MIN_LVL, PokemonInstance.MAX_LVL))
 		else:
 			self._level = d
 
@@ -162,17 +353,29 @@ class PokemonInstance:
 	def _validate_lvl(self, d):
 		return not (d < PokemonInstance.MIN_LVL or d > PokemonInstance.MAX_LVL)
 
-	def _validate_ev_ttl(self, d):
-		return (self.hpev + self.atkev + self.dfnev + self.satkev + self.sdfnev + self.spdev) <= PokemonInstance.MAX_EVS_TOTAL
+	def _validate_ev_ttl(self, hp_ev=None, atk_ev=None, dfn_ev=None, satk_ev=None, sdfn_ev=None, spd_ev=None):
+		if hp_ev is None:
+			hp_ev = self.hp_ev if hasattr(self, 'hp_ev') else 0
+		if atk_ev is None:
+			atk_ev = self.atk_ev if hasattr(self, 'atk_ev') else 0
+		if dfn_ev is None:
+			dfn_ev = self.dfn_ev if hasattr(self, 'dfn_ev') else 0
+		if satk_ev is None:
+			satk_ev = self.satk_ev if hasattr(self, 'satk_ev') else 0
+		if sdfn_ev is None:
+			sdfn_ev = self.sdfn_ev if hasattr(self, 'sdfn_ev') else 0
+		if spd_ev is None:
+			spd_ev = self.spd_ev if hasattr(self, 'spd_ev') else 0
+		return (hp_ev + atk_ev + dfn_ev + satk_ev + sdfn_ev + spd_ev) <= PokemonInstance.MAX_EVS_TOTAL
 
 	def _validate_ev_val(self, d):
-		return d <= PokemonInstance.MAX_EVS_PER_STAT
+		return 0 <= d <= PokemonInstance.MAX_EVS_PER_STAT
 
 	def _validate_iv_val(self, d):
-		return not (d < PokemonInstance.MIN_BST_VAL or d > PokemonInstance.MAX_BST_VAL)
+		return not (d < 0 or d > PokemonInstance.MAX_IVS)
 
 	def _calculate_stat(self, iv, bsv, evs, lvloffset=0, foffset=5, nature=0):
-		return ((iv + 2* bsv + evs/4 + lvloffset) * self.level / 100 + foffset) * (1.0 + nature * 0.1)
+		return floor( floor((iv + 2* bsv + evs/4 + lvloffset) * self.level / 100 + foffset) * (1.0 + nature * 0.1) )
 
 	def attacks_with(self, technique, *targets):
 		damages = []
--- tests/pokemon_creation.py
+++ tests/pokemon_creation.py
@@ -1,7 +1,7 @@
 import unittest
 
 import fkmnkit
-from fkmnkit.creatures import Pokemon
+from fkmnkit.creatures import Pokemon, PokemonInstance, Nature, StatID
 
 class TestPokemonCreation(unittest.TestCase):
 	def test_bst_validation(self):
@@ -10,6 +10,83 @@ class TestPokemonCreation(unittest.TestCase):
 
 		actual_pokemon = Pokemon(bhp=200)
 		self.assertEquals(actual_pokemon.bhp, 200)
+
+	def test_iv_validation(self):
+		actual_pokemon = Pokemon(bhp=200)
+
+		with self.assertRaises(ValueError):
+			PokemonInstance(actual_pokemon, hp_iv=32)
+
+		with self.assertRaises(ValueError):
+			PokemonInstance(actual_pokemon, hp_iv=-1)
+
+		actual_pokemoni = PokemonInstance(actual_pokemon, hp_iv=16)
+		self.assertEquals(actual_pokemoni.hp_iv, 16)
+
+	def test_ev_validation(self):
+		actual_pokemon = Pokemon(bhp=200)
+
+		with self.assertRaises(ValueError):
+			PokemonInstance(actual_pokemon, hp_ev=253)
+
+		with self.assertRaises(ValueError):
+			PokemonInstance(actual_pokemon, hp_ev=168, atk_ev=168, dfn_ev=168, spd_ev=168)
+
+		with self.assertRaises(ValueError):
+			PokemonInstance(actual_pokemon, hp_ev=-1)
+
+		actual_pokemoni = PokemonInstance(actual_pokemon, hp_ev=168)
+		self.assertEquals(actual_pokemoni.hp_ev, 168)
+
+		actual_pokemoni = PokemonInstance(actual_pokemon, hp_ev=168, atk_ev=168, dfn_ev=168, spd_ev=6)
+		self.assertEquals(actual_pokemoni.hp_ev, 168)
+		self.assertEquals(actual_pokemoni.atk_ev, 168)
+		self.assertEquals(actual_pokemoni.dfn_ev, 168)
+		self.assertEquals(actual_pokemoni.spd_ev, 6)
+
+	def test_hp_stat_calculation(self):
+		wigglytuff = Pokemon(bhp=140, batk=70, bdfn=45, bsatk=85, bsdfn=50, bspd=45)
+		wigglytuff_instance = PokemonInstance(wigglytuff)
+
+		wigglytuff_instance.level = 50
+		self.assertEquals(wigglytuff_instance.hp, 200)
+
+		wigglytuff_instance.level = 100
+		self.assertEquals(wigglytuff_instance.hp, 390)
+
+		wigglytuff_instance.hp_ev = 252
+		wigglytuff_instance.hp_iv = 31
+		self.assertEquals(wigglytuff_instance.hp, 484)
+
+	def test_other_stat_calculation(self):
+		wigglytuff = Pokemon(bhp=140, batk=70, bdfn=45, bsatk=85, bsdfn=50, bspd=45)
+		hardy = Nature(StatID.ATK, StatID.ATK)
+		wigglytuff_instance = PokemonInstance(wigglytuff, nature=hardy)
+
+		wigglytuff_instance.level = 50
+		self.assertEquals(wigglytuff_instance.atk, 75)
+
+		wigglytuff_instance.level = 100
+		self.assertEquals(wigglytuff_instance.atk, 145)
+
+		wigglytuff_instance.atk_ev = 252
+		wigglytuff_instance.atk_iv = 31
+		self.assertEquals(wigglytuff_instance.atk, 239)
+
+	def test_other_stat_calculation_with_nature(self):
+		wigglytuff = Pokemon(bhp=140, batk=70, bdfn=45, bsatk=85, bsdfn=50, bspd=45)
+		adamant = Nature(StatID.ATK, StatID.SATK)
+		wigglytuff_instance = PokemonInstance(wigglytuff, nature=adamant)
+
+		wigglytuff_instance.level = 50
+		self.assertEquals(wigglytuff_instance.atk, 82)
+
+		wigglytuff_instance.level = 100
+		self.assertEquals(wigglytuff_instance.atk, 159)
+
+		wigglytuff_instance.atk_ev = 252
+		wigglytuff_instance.atk_iv = 31
+		self.assertEquals(wigglytuff_instance.atk, 262)
 
 if __name__ == '__main__':
 	unittest.main()
